7c10c4f8ae0741ef5249ccacb784a48930d4050d
eduardacosta-design <eduarda.costa.pinto@escola.pr.gov.br>
2026-07-06 12:01:48 -0300

    l,k

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelector
+console.log(botoes)
+
+ botoes.array.forEach( function (botao) {
+    let curtiu = false
+    botao. addEventListener ("click" ,
+
+        function botaoClicado() {
+            console.log ("botao Clicado")
+        }
+ })
